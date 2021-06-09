74606759684935a610abce9402952024ff00ad61
Boris Kotsev <bobiko2319abv@gmail.com>
2021-06-09 19:41:00 +0300

    .

---
+++ include/Map/City.h
@@ -0,0 +1,28 @@
+#ifndef CITY_H
+#define CITY_H
+
+#include <iostream>
+#include <fstream>
+#include <SDL.h>
+
+using namespace std;
+
+class City
+{
+public:
+    City();
+    City(const City& model);
+    virtual ~City();
+
+    SDL_Rect m_objRect;
+    SDL_Texture* m_cityTexture;
+
+    void draw();
+    void load(string configFile);
+
+protected:
+
+private:
+};
+
+#endif // CITY_H
