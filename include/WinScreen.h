4413141ae98931261947cdb735f4972bb97ac599
Boris Kotsev <bobiko2319abv@gmail.com>
2021-06-12 09:25:09 +0300

    new Map

--- include/WinScreen.h
+++ include/WinScreen.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <fstream>
+#include <SDL.h>
+
 #include "defines.h"
 
 class WinScreen
@@ -7,7 +10,13 @@ class WinScreen
 public:
 	WinScreen();
 	virtual ~WinScreen();
+
+	SDL_Rect m_ButtonRect;
+	SDL_Texture* m_winTexture;
+	SDL_Texture* m_loseTexture;
 	
 	void init(string configFile, OWNER owner);
+	void draw();
+	void update();
 
 };
--- src/WinScreen.cpp
+++ src/WinScreen.cpp
@@ -13,5 +13,16 @@ WinScreen::~WinScreen()
 
 }
 
+void WinScreen::init(string configFile, OWNER owner)
+{
+	configFile = "config\\" + configFile;
 
+	fstream stream;
+	string tmp, winImg, loseImg;
+
+	stream.open(configFile.c_str());
+	
+
+	stream.close();
+}
 
